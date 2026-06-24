6041ed200100d43c0f1da8ec97ffdc39b7c67ec7
Deepak singh <studydipu@gmail.com>
2026-06-24 18:38:03 +0530

    added a env

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express=require('express');
+const { env } = require('node:process');
+const dotenv=require('dotenv');
+dotenv.config();
 const app=express();
+
+
 app.get('/home',(req,res)=>{
     res.json({message:'OK'});
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Started the server")
 })
